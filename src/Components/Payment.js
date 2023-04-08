0ba0a4a6f983977186edfa638a5bcac10b4a5f46
Md. Shehab Chowdhury <shehab15-11396@diu.edu.bd>
2023-04-08 20:14:03 +0600

    payment info done

--- src/Components/Payment.js
+++ src/Components/Payment.js
@@ -1,7 +1,124 @@
-import React from "react";
+import { Input } from "@material-tailwind/react";
+import React, { useState } from "react";
 
 const Payment = () => {
-  return <div></div>;
+  const [cardNumber, setCardNumber] = useState("");
+  const [cardName, setCardName] = useState("");
+  const [expiryDate, setExpiryDate] = useState("");
+  const [cvv, setCvv] = useState("");
+  const [zipCode, setZipCode] = useState("");
+
+  const handleCardNumberChange = (event) => {
+    setCardNumber(event.target.value);
+  };
+
+  const handleCardNameChange = (event) => {
+    setCardName(event.target.value);
+  };
+
+  const handleExpiryDateChange = (event) => {
+    setExpiryDate(event.target.value);
+  };
+
+  const handleCvvChange = (event) => {
+    setCvv(event.target.value);
+  };
+
+  const handleZipCodeChange = (event) => {
+    setZipCode(event.target.value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    // Add code to handle form submission here
+  };
+
+  return (
+    <div>
+      <div className="min-h-screen bg-gray-100 rounded-none">
+        <div className="max-w-7xl mx-auto py-6 sm:px-6 lg:px-8 "></div>
+        <div className="px-4 py-6 sm:px-0"></div>
+        <div className="bg-white shadow overflow-hidden sm:rounded-lg md:w-2/4 w-3/4 py-10 px-5	mx-auto rounded-none">
+          <div className="px-4 py-5 sm:px-6">
+            <h3 className="text-lg leading-6 font-medium text-gray-900">
+              Payment Information
+            </h3>
+          </div>
+          <div className="border-t border-gray-200 px-4 py-5 sm:p-0 ">
+            <form onSubmit={handleSubmit}>
+              <div className="grid grid-cols-1 gap-6">
+                <div>
+                  <label
+                    htmlFor="cardNumber"
+                    className="block text-sm font-medium text-gray-700"
+                  >
+                    Card number
+                  </label>
+                  <Input
+                    type="text"
+                    name="cardNumber"
+                    id="cardNumber"
+                    value={cardNumber}
+                    onChange={handleCardNumberChange}
+                    placeholder="1234 1234 1234 1234"
+                    autoComplete="cc-number"
+                    className="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
+                  />
+                </div>
+
+                <div>
+                  <label
+                    htmlFor="expiryDate"
+                    className="block text-sm font-medium text-gray-700"
+                  >
+                    Expiry date
+                  </label>
+                  <Input
+                    type="text"
+                    name="expiryDate"
+                    id="expiryDate"
+                    value={expiryDate}
+                    placeholder="1234"
+                    onChange={handleExpiryDateChange}
+                    autoComplete="cc-exp"
+                    className="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
+                  />
+                </div>
+
+                <div>
+                  <label
+                    htmlFor="cvv"
+                    className="block text-sm font-medium text-gray-700"
+                  >
+                    CVV
+                  </label>
+                  <Input
+                    type="text"
+                    name="cvv"
+                    id="cvv"
+                    placeholder="1234"
+                    value={cvv}
+                    onChange={handleCvvChange}
+                    autoComplete="cc-csc"
+                    className="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
+                  />
+                </div>
+              </div>
+
+              <div className="mt-6">
+                <button
+                  type="submit"
+                  className="inline-flex items-center px-4 py-2 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                >
+                  Pay
+                </button>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Payment;

e3e89bd7514d3abac0821e2feba7997d1c6107de
Md. Shehab Chowdhury <shehab15-11396@diu.edu.bd>
2023-04-08 21:00:21 +0600

    all done

--- src/Components/Error.js
+++ src/Components/Error.js
@@ -1,7 +1,13 @@
 import React from "react";
+import error from "../Assets/T-rex.json";
+import Lottie from "lottie-react";
 
 const Error = () => {
-  return <div></div>;
+  return (
+    <div className="w-2/5		mx-auto ">
+      <Lottie animationData={error} />
+    </div>
+  );
 };
 
 export default Error;
